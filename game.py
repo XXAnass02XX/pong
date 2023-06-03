d5eb0401c33e6c6cb09284e10e52a7c23e8d9bad
haydara <core.editor>
2023-06-03 23:39:47 +0200

    drawing the pads

--- game.py
+++ game.py
@@ -1,1 +1,54 @@
 #!/usr/bin/env python3
+import pygame 
+pygame.init()
+
+WIDTH,HEIGHT = 800,800
+PADDLE_WIDTH = WIDTH//20
+PADDLE_HEIGHT = HEIGHT//7
+MAX_FPS = 60
+WHITE =(255,255,255)
+BLACK = (0,0,0)
+
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+clock = pygame.time.Clock()
+pygame.display.set_caption("pong game")
+
+class Paddle:
+    COLOR = WHITE
+    
+    def __init__(self,x,y,width,height):
+        '''the position and size of the paddle
+        the x and y are the coordinates of the topleft
+        of the rectangle'''
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    def draw(self,window):
+        pygame.draw.rect(window,self.COLOR,(self.x,self.y,self.width,self.height))
+
+def fill_window(window,paddles):
+    '''draw the image in the window'''
+    window.fill(BLACK)
+    for paddle in paddles:
+        paddle.draw(window)
+    pygame.display.update()
+
+def main():
+    '''main fonc that displays the game window'''
+    running= True
+    left_paddle = Paddle(5,HEIGHT//2-PADDLE_HEIGHT//2,PADDLE_WIDTH,PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH-PADDLE_WIDTH-5,HEIGHT//2-PADDLE_HEIGHT//2,PADDLE_WIDTH,PADDLE_HEIGHT)
+    padles = (left_paddle,right_paddle)
+    while running:
+        fill_window(window,padles)
+        clock.tick(MAX_FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running= False
+                break
+    
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
